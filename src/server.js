2682327860004a57fc3514bf84c02f4871ea25ee
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2023-08-20 23:03:42 -0300

    testando com express

--- src/server.js
+++ src/server.js
@@ -1,27 +1,9 @@
-import http from 'node:http'
-import { json } from './middlewares/json.js'
+import express from 'express'
 
+const app = express()
 
-const server = http.createServer(async (req, res) => {
-  const { method, url } = req
-
-  await json(req, res)
-
-  if (method === 'GET' && url === '/users') {
-    return res.end(JSON.stringify({ user: "user" }))
-  }
-
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
-
-    const user = {
-      id: 1,
-      name,
-      email,
-    }
-
-    return res.end(JSON.stringify(user))
-  }
-  return res.writeHead(404).end()
+app.use('/', (req, res) => {
+  return res.send({ user: "deu certo" })
 })
-server.listen(3333)
+
+app.listen(3333, console.log('server is running'))
