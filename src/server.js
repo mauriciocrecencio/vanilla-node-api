726ed876a66d8fcfb91ebd88532c96c06fe83b33
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2023-08-20 22:48:21 -0300

    deploy

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 import http from 'node:http'
-import { Database } from './database.js'
 import { json } from './middlewares/json.js'
 
-const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
@@ -10,9 +8,7 @@ const server = http.createServer(async (req, res) => {
   await json(req, res)
 
   if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
-
-    return res.end(JSON.stringify(users))
+    return res.end(JSON.stringify({ user: "user" }))
   }
 
   if (method === 'POST' && url === '/users') {
@@ -24,9 +20,7 @@ const server = http.createServer(async (req, res) => {
       email,
     }
 
-    database.insert('users', user)
-
-    return res.writeHead(201).end()
+    return res.end(JSON.stringify(user))
   }
   return res.writeHead(404).end()
 })
